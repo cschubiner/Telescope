7c9bd428f622e9b4bdaa29a131394c78e25c3c62
Clay Schubiner <cschubiner@gmail.com>
2015-02-24 17:19:01 -0800

    new background / package

---
+++ packages/telescope-theme-kepler/package.js
@@ -0,0 +1,14 @@
+Package.describe({summary: "Telescope Kepler theme"});
+
+Package.on_use(function (api) {
+
+  api.use(['fourseven:scss','templating', 'telescope-base', 'telescope-theme-hubble'], ['client']);
+  // api.use([, 'telescope-theme-hubble'], ['client']);
+
+  api.addFiles([
+    'lib/client/stylesheets/screen.scss',
+    'lib/client/templates/new_posts_list.html',
+    'lib/client/kepler.js',
+    ], ['client']);
+
+});
