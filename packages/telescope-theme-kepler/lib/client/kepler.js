d0c616d7dc22e47913550745644d02dbf7c63d42
Clay Schubiner <cschubiner@gmail.com>
2015-03-22 18:07:41 -1000

    stuff

--- packages/telescope-theme-kepler/lib/client/kepler.js
+++ packages/telescope-theme-kepler/lib/client/kepler.js
@@ -1,15 +1,15 @@
 templates["posts_list"] = "newPostsList";
 
-var customProperty = {
-  propertyName: 'customProperty',
-  propertySchema: {
-    type: String,                           // property type
-    label: 'customLabel',                   // key string used for internationalization
-    optional: true,                         // make this property optional
-    autoform: {
-      editable: true,                       // make this property editable by users
-      type: "bootstrap-datetimepicker"      // assign a custom input type
-    }
-  }
-}
-addToPostSchema.push(customProperty);
+// var customProperty = {
+//   propertyName: 'customProperty',
+//   propertySchema: {
+//     type: String,                           // property type
+//     label: 'customLabel',                   // key string used for internationalization
+//     optional: true,                         // make this property optional
+//     autoform: {
+//       editable: true,                       // make this property editable by users
+//       type: "bootstrap-datetimepicker"      // assign a custom input type
+//     }
+//   }
+// }
+// addToPostSchema.push(customProperty);
--- packages/telescope-theme-kepler/package.js
+++ packages/telescope-theme-kepler/package.js
@@ -3,7 +3,6 @@ Package.describe({summary: "Telescope Kepler theme"});
 Package.on_use(function (api) {
 
   api.use(['fourseven:scss','templating', 'telescope-base', 'telescope-theme-hubble'], ['client']);
-  // api.use([, 'telescope-theme-hubble'], ['client']);
 
   api.addFiles([
     'lib/client/stylesheets/screen.scss',
