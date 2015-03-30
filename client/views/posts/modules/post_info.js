2f45e426f16d909f054a224ea63f23d479e484eb
Clay Schubiner <cschubiner@gmail.com>
2015-03-29 19:09:19 -0700

    uproots and down votes

--- client/views/posts/modules/post_info.js
+++ client/views/posts/modules/post_info.js
@@ -1,6 +1,6 @@
 Template[getTemplate('postInfo')].helpers({
   pointsUnitDisplayText: function(){
-    return this.upvotes == 1 ? i18n.t('point') : i18n.t('points');
+    return this.baseScore == 1 ? i18n.t('point') : i18n.t('points');
   },
   getTemplate: function() {
     return getTemplate("postAuthor");
--- client/views/posts/modules/post_upvote.js
+++ client/views/posts/modules/post_upvote.js
@@ -3,11 +3,21 @@ Template[getTemplate('postUpvote')].helpers({
     var user = Meteor.user();
     if(!user) return false;
     return _.include(this.upvoters, user._id);
+  },
+  downvoted: function(){
+    var user = Meteor.user();
+    if(!user) return false;
+    return _.include(this.downvoters, user._id);
+  },
+  voted: function(){
+    var user = Meteor.user();
+    if(!user) return false;
+    return  _.include(this.upvoters, user._id) || _.include(this.downvoters, user._id);
   }
 });
 
 Template[getTemplate('postUpvote')].events({
-  'click .upvote-link': function(e, instance){
+  'click .upvotelinkclay': function(e, instance){
     var post = this;
     e.preventDefault();
     if(!Meteor.user()){
@@ -17,5 +27,30 @@ Template[getTemplate('postUpvote')].events({
     Meteor.call('upvotePost', post, function(error, result){
       trackEvent("post upvoted", {'_id': post._id});
     });
-  }
+  },
+  'click .downvotelinkclay': function(e, instance){
+    var post = this;
+    e.preventDefault();
+    if(!Meteor.user()){
+      Router.go('atSignIn');
+      flashMessage(i18n.t("please_log_in_first"), "info");
+    }
+    Meteor.call('downvotePost', post, function(error, result){
+      trackEvent("post downvoted", {'_id': post._id});
+    });
+  },
+  'click .unvotelinkclay': function(e, instance){
+    var post = this;
+    e.preventDefault();
+    if(!Meteor.user()){
+      Router.go('atSignIn');
+      flashMessage(i18n.t("please_log_in_first"), "info");
+    }
+    Meteor.call('cancelUpvotePost', post, function(error, result){
+      trackEvent("post cancelUpvotePost", {'_id': post._id});
+    });
+    Meteor.call('cancelDownvotePost', post, function(error, result){
+      trackEvent("post cancelDownvotePost", {'_id': post._id});
+    });
+  },
 });
